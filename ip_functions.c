714f7caa579ded8f7b6ac560a426c4c01a46b141
andreabyte <andre24@alice.it>
2021-06-12 17:13:40 +0200

    Test on ip functions

---
+++ ip_functions.c
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <arpa/inet.h>
+#include <math.h>
+
+#define PREFIX_LEN  15  /*Max length of IP address in A.B.C.D format*/
+#define MAX_MASK_LEN 32  /*Maximum mask value in decimal notation*/
+
+
